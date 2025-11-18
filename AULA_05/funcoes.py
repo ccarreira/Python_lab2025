50d3e663d3883641cdf985f584fa4198a201058c
ccarreira <cesar.carreira@gmail.com>
2025-11-18 14:42:35 +0000

    ex funcoes

--- AULA_05/funcoes.py
+++ AULA_05/funcoes.py
@@ -30,22 +30,76 @@ def execute(fnt, value="None"):
     #for v in values: 
 
 
-execute_fnt = defesa
+action = defesa
 value = "escudo"
 
-print(execute(execute_fnt, value))
+print(execute(action, value))
 
 #2
 actions=[ataque, defesa, cura, abrir_inventario, fugir]
 
 for a in actions:
-    print(execute(a, ""))
+    print(execute(a, "escudo"))
 
 
 #3
 
-def execute_n(action, nTimes=1):
+def execute_n(action, value, nTimes=1):
     for n in range(nTimes):
-        print(execute(action))
+        print(execute(action, value))
 
-execute_n(ataque, 3)
+execute_n(ataque, "espada", 2)
+
+#4
+
+def attack():
+    return "ataca"
+def defend():
+    return "defende"
+def rest():
+    return "descansa"
+
+dicionario_de_acoes = {
+    "ataque" : attack,
+    "defesa" : defend,
+    "descansa": rest
+}
+
+
+def do(action):
+    print(dicionario_de_acoes[action]())
+
+do("ataque")
+
+
+#5
+
+dicionario_de_acoes = {
+    "ataque" : ataque,
+    "defesa" : defesa,
+    "cura": cura,
+    "inventario" : abrir_inventario,
+    "fugir" : fugir,
+}
+
+
+def execute2(action, value="None"):
+    action = str(action)
+    if action in dicionario_de_acoes:
+        print("RESULTADO #5: " + dicionario_de_acoes[action](value))
+    else:    
+        print("Acao desconhecida_execute", action)
+
+
+comando = False
+while not comando:
+    action = str(input("qual a acao? : "))
+    if action in dicionario_de_acoes:
+        comando = True
+    else:
+        print("Acao desconhecida", action)
+
+execute2(action)
+
+
+
